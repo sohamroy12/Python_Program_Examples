2c064ca05d199eff31b462fb009872b23d00b4f3
Soham Roy <sohamroy52@gmail.com>
2025-06-07 18:43:12 +0530

    Adding multiple lines to the pages

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ for item, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=22)
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, txt=row["Topic"], h=12, align="L", ln=1)
-    pdf.line(10, 20, 200, 20)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set Footer
     pdf.ln(254)
@@ -25,6 +26,8 @@ for item, row in df.iterrows():
 
     for i in range(row["Pages"] -1):
         pdf.add_page()
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # Set Header
         # pdf.set_font(family="Times", style="B", size=22)
@@ -33,7 +36,7 @@ for item, row in df.iterrows():
         # pdf.line(10, 20, 200, 20)
 
         # Set Footer
-        pdf.ln(266)
+        pdf.ln(268)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, txt=row["Topic"], h=12, align="R")
