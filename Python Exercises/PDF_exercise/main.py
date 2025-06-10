a03850f7e114e64bf81283be8bb3cb3f2af58a17
Soham Roy <sohamroy52@gmail.com>
2025-06-11 01:45:32 +0530

    Read from text file and add it as a multile line cell to the PDF

--- Python Exercises/PDF_exercise/main.py
+++ Python Exercises/PDF_exercise/main.py
@@ -8,11 +8,15 @@ pdf.set_auto_page_break(auto=False, margin=0)
 filePaths = glob.glob(f"Text+Files/*.txt")
 
 for files in filePaths:
+    with open(files) as f:
+        text = f.read()
     pdf.add_page()
     fileName = Path(files).stem
     name = fileName.title()
         
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{name}")
+    pdf.cell(w=50, h=8, txt=f"{name}", ln=1)
+    pdf.set_font(family="Times", size=11)
+    pdf.multi_cell(w=0, h=8, txt=text)
     
 pdf.output(f"PDFs/All.pdf")
